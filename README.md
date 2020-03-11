1dd0b4336eaa4775edb2a0d6aaab6e052301916f
Chris533 <eskim0@qq.com>
2020-03-11 22:55:59 +0800

    Prettier fix

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # bunadmin-plugin-buncms-file
 
 ### how to use
+
 ```
 cd bunadmin/plugins/
 git clone https://github.com/Chris533/bunadmin-plugin-buncms-file.git buncms-file
--- media/services/uploadFileSer.tsx
+++ media/services/uploadFileSer.tsx
@@ -1,11 +1,11 @@
 import storedToken from "@/utils/scripts/storedToken"
 import request from "umi-request"
-import {ENV} from "@/utils/config"
+import { ENV } from "@/utils/config"
 
 interface Resp {
   id?: string // media id
-  file_name?:     string
-  file_size?:     number
+  file_name?: string
+  file_size?: number
   media_name?: string
   type?: string
   title?: string
